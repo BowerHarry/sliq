9e01aae641a0eeacde97c2b1cbb4805546f80ccc
Harry Bower <BowerHarry@users.noreply.github.com>
2024-02-27 00:02:21 +0000

    Added vertical tile animation and border rotation
    Required large code restructure. Border rotation animation is first draft.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-
+from kivy.uix.image import Image
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.properties import (
     NumericProperty, ObjectProperty
@@ -30,7 +30,26 @@ class BoardTile(ButtonBehavior, RelativeLayout):
     gridY=NumericProperty()
     v=NumericProperty()
     clicked_inside = BooleanProperty(False)
-    board_instance = ObjectProperty(None) 
+    board_instance = ObjectProperty(None)
+
+    def __init__(self, x, y, v, b, **kwargs):
+        self.s='src/tile.png'
+        if v in range (0,5): self.s='src/' + str(v) + '-tile.png'
+        img = Image(source = self.s)
+        img.size_hint_x = None
+        img.size_hint_y = None
+        img.size_x = 100
+        img.size_y = 100
+        self.x = x*100
+        self.y = y*100
+        self.gridX = x
+        self.gridY = y
+        self.v = v
+        self.board_instance = b
+        self.ids = {'x': x, 'y': y, 'value': v, 'object':'tile'}
+        super(BoardTile, self).__init__(**kwargs)
+        self.add_widget(img)
+
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
@@ -64,55 +83,80 @@ class TileHorizontalEdge(RelativeLayout):
     x=NumericProperty()
     y=NumericProperty()
 
-class Border:
-    def __init__(self, length):
-        self.length = length
-        self.grid = [[None for x in range(length)] for y in range(length)]
+
+class Border(RelativeLayout):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.length = 8+2
+        self.grid = [[None for x in range(self.length)] for y in range(self.length)]
         grid = self.grid
         for i in range(len(grid[0])):
             grid[0][i] = self.randomValue()
-            grid[length-1][i] = self.randomValue()
+            grid[self.length-1][i] = self.randomValue()
             grid[i][0] = self.randomValue()
-            grid[i][length-1] = self.randomValue()
+            grid[i][self.length-1] = self.randomValue()
+
+        self.addBorderTiles()
+
+    def rotate(self):
+        self.animateRotation()
+        self.rotateBorderGrid()
+
+    def animateRotation(self, *kwargs):
+        anim = Animation(angle=self.angle - 90, duration=1)
+        anim.start(self)
 
     def randomValue(self):
         return random.randint(1,4)
     
-    def print(self):
-        g = self.grid
-        grid = [[None for x in range(self.length)] for y in range(self.length)]
+    def rotateBorderGrid(self):
+        self.grid = list(zip(*self.grid[::-1]))
+
+    def addBorderTiles(self):
         for i in range(self.length):
             for j in range(self.length):
-                if g[i][j] == None:
-                    grid[i][j] = 0
+                if (i == 0 and j==0) or (i == self.length-1 and j == 0) or (i == 0 and j == self.length-1) or (i == self.length-1 and j == self.length-1):
+                    # ignore corners
+                    pass
                 else:
-                    grid[i][j] = g[i][j]
-        
-        for i in grid:
-            print(i)
+                    if i == 0 or i == self.length-1:
+                        s='src/' + str(self.grid[i][j]) + '-vertical-edge.png'
+                        if i == self.length-1:
+                            x = i-1
+                        else:
+                            x = i
+                        self.add_widget(TileVerticalEdge(x=90+(x*100), y=(j*100), ids={'x': i, 'y': j, 'value':self.grid[i][j], 'object':'border'}, s=s))
+                    if j == 0 or j == self.length-1:
+                        if j == self.length-1:
+                            y = j-1
+                        else:
+                            y = j
+                        s='src/' + str(self.grid[i][j]) + '-horizontal-edge.png'
+                        self.add_widget(TileHorizontalEdge(x=(i*100), y=90+(y*100), ids={'x': i, 'y': j, 'value':self.grid[i][j], 'object':'border'}, s=s))
 
-    def rotate(self):
-        self.grid = list(zip(*self.grid[::-1]))
 
 class Board(RelativeLayout):
     length = 8+2
-    border = Border(length)
     game_instance = ObjectProperty(None)
     score = NumericProperty(0)
     turn = NumericProperty(1)
+    border = ObjectProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # self.border = Border(self.length)
         for i in range(1,self.length-1):
             for j in range (1,self.length-1):
-                self.add_widget(BoardTile(x=(i*100), y=(j*100), ids={'x': i, 'y': j, 'value':-1, 'object':'tile'}, s='src/tile.png', v=-1, gridX= i, gridY=j, board_instance=self))
+                newTile = BoardTile(i, j, -1, self)
+                self.add_widget(newTile)
         self.fillWithStartingGrid()
-        print(self.newTile())
-        print(self.newTile())
-        print(self.newTile())
-        print(self.newTile())
-        self.drawBorder()
-        self.update()
+        self.newTile()
+        self.newTile()
+        self.newTile()
+        self.newTile()
+        Clock.schedule_once(lambda dt: self.update())
+        
 
     def yield_to_sleep(func):
         @functools.wraps(func)
@@ -132,11 +176,12 @@ class Board(RelativeLayout):
     def moveTile(self, x, y, value, direction, tile):
         if value != 0:
             if direction == "RIGHT":
-                if self.isBorder(x+1, y) != -1 and self.isBorder(x+1, y) == value:
-                    self.score += value
-                    self.remove(x, y)
-                    self.gravity(x)
-                    self.update()
+                if self.isBorder(x+1, y) != None:
+                    if self.isBorder(x+1, y) == value:
+                        self.score += value
+                        self.remove(x, y)
+                        self.gravity(x)
+                        self.update()
                     print("BORDER TILE!")
                 elif self.isEmpty(x+1, y):
                     end_x, end_y = self.calcGravity(x+1, y)
@@ -146,16 +191,17 @@ class Board(RelativeLayout):
                     yield y_duration + 0.1
                     print("SOUNDS GOOD")
                     self.remove(x, y)
-                    self.addWithGravity(x+1, y, value-1)
+                    self.add(end_x, end_y, value-1)
                     self.update()
                 else:
                     print("CAN'T MOVE THERE")
             elif direction == "LEFT":
-                if self.isBorder(x-1, y) != -1 and self.isBorder(x-1, y) == value:
-                    self.score += value
-                    self.remove(x, y)
-                    self.gravity(x)
-                    self.update()
+                if self.isBorder(x-1, y) != None:
+                    if self.isBorder(x-1, y) == value:
+                        self.score += value
+                        self.remove(x, y)
+                        self.gravity(x)
+                        self.update()
                     print("BORDER TILE!")
                 elif self.isEmpty(x-1, y):
                     end_x, end_y = self.calcGravity(x-1, y)
@@ -165,7 +211,7 @@ class Board(RelativeLayout):
                     yield y_duration + 0.1
                     print("SOUNDS GOOD")
                     self.remove(x, y)
-                    self.addWithGravity(x-1, y, value-1)
+                    self.add(end_x, end_y, value-1)
                     self.update()
                 else:
                     print("CAN'T MOVE THERE")
@@ -177,22 +223,26 @@ class Board(RelativeLayout):
         yOrder = 1
         while yOrder < self.length-1:
             for child in self.children:
-                if (child.ids.x) == x and (child.ids.y) == yOrder and (child.ids.value) != -1:
-                    self.remove(x, child.ids.y)
-                    self.addWithGravity(child.ids.x, child.ids.y, child.ids.value)
+                if (not isinstance(child, Border)):
+                    if (child.ids.x) == x and (child.ids.y) == yOrder and (child.ids.value) != -1:
+                        self.remove(x, child.ids.y)
+                        end_x, end_y = self.calcGravity(x, child.ids.y)
+                        self.add(end_x, end_y, child.ids.value)
             yOrder+=1
 
+    @yield_to_sleep
     def rotateBorder(self):
         self.border.rotate()
-        self.removeBorder()
-        self.drawBorder()
-        print(self.newTile())
-        print(self.newTile())
-        print(self.newTile())
+        yield(1)
+        self.update()
+        self.newTile()
+        self.newTile()
+        self.newTile()
         for i in range(math.ceil(self.turn/2)):
             print(self.newTile())
         self.update()
         self.turn += 1
+
 
     def fillWithStartingGrid(self):
         self.add(1, 1, 4)
@@ -202,34 +252,6 @@ class Board(RelativeLayout):
         self.add(6, 1, 3)
         self.add(6, 2, 4)
         self.add(7, 1, 4)
-
-    def removeBorder(self):
-        for child in self.children:
-            if (child.ids.x) == 0 or (child.ids.y) == self.length-1 or (child.ids.y) == 0 or (child.ids.x) == self.length-1:
-                self.remove_widget(child)
-    
-    def drawBorder(self):
-        for i in range(self.length):
-            for j in range(self.length):
-                if (i == 0 and j==0) or (i == self.length-1 and j == 0) or (i == 0 and j == self.length-1) or (i == self.length-1 and j == self.length-1):
-                    # ignore corners
-                    pass
-                else:
-                    if i == 0 or i == self.length-1:
-                        s='src/' + str(self.border.grid[i][j]) + '-vertical-edge.png'
-                        if i == self.length-1:
-                            x = i-1
-                        else:
-                            x = i
-                        self.add_widget(TileVerticalEdge(x=90+(x*100), y=(j*100), ids={'x': i, 'y': j, 'value':self.border.grid[i][j], 'object':'border'}, s=s))
-                    if j == 0 or j == self.length-1:
-                        if j == self.length-1:
-                            y = j-1
-                        else:
-                            y = j
-                        s='src/' + str(self.border.grid[i][j]) + '-horizontal-edge.png'
-                        self.add_widget(TileHorizontalEdge(x=(i*100), y=90+(y*100), ids={'x': i, 'y': j, 'value':self.border.grid[i][j], 'object':'border'}, s=s))
-
     
     def randomValue(self):
         return random.randint(1,4)
@@ -246,81 +268,75 @@ class Board(RelativeLayout):
                 y = i
                 return x, y
             
-    def addWithGravity(self, x, y, v):
-        for i in range(y, 0, -1):
-            if self.isEmpty(x, i) == False:
-                y = i+1
-                self.add(x, y, v)
-                return("\nNew tile at (" + str(x) + "," + str(y) + ")")
-                
-            elif i == 1:
-                y = i
-                self.add(x, y, v)
-                return("\nNew tile at (" + str(x) + "," + str(y) + ")")
 
+    @yield_to_sleep
     def newTile(self):
         x = self.randomX()
         y = self.length-2
         v = self.randomValue()
-        for i in range(self.length-2, 0, -1):
-            if self.isEmpty(x, i) == False:
-                y = i+1
-                self.add(x, y, v)
-                return("\nNew tile at (" + str(x) + "," + str(y) + ")")
-                
-            elif i == 1:
-                y = i
-                self.add(x, y, v)
-                return("\nNew tile at (" + str(x) + "," + str(y) + ")")
-        return ("failed")
+        tile = self.add(x, y, v)
+        end_x, end_y = self.calcGravity(x, y-1)
+        y_duration = 0.2 * abs(y-end_y)
+        anim = Animation(x=end_x*100, y=y*100, duration=0.2) + Animation(x=end_x*100, y=end_y*100, duration = y_duration)
+        anim.start(tile)
+        yield y_duration + 0.1
+        self.remove(x, y)
+        self.add(end_x, end_y, v)
+        self.update()
+        print("\nNew tile at (" + str(end_x) + "," + str(end_y) + ")")
     
     def isBorder(self, x, y):
-        for child in self.children:
-            if (child.ids.x) == x and (child.ids.y) == y and (child.ids.object) == "border":
-                return child.ids.value
-        return -1
+        # print(self.border.grid[x][y])
+        return self.border.grid[x][y]
 
 
     def isEmpty(self, x, y):
         for child in self.children:
-            if (child.ids.x) == x and (child.ids.y) == y and (child.ids.value) != -1:
-                return False
+            if (not isinstance(child, Border)):
+                if (child.ids.x) == x and (child.ids.y) == y and (child.ids.value) != -1:
+                    return False
         return True
     
     
 
     def remove(self, x, y):
         for child in self.children:
-            if (child.ids.x) == x and (child.ids.y) == y and (child.ids.value) != -1:
-                self.remove_widget(child)
+            if (not isinstance(child, Border)):
+                if (child.ids.x) == x and (child.ids.y) == y and (child.ids.value) != -1:
+                    self.remove_widget(child)
 
     def add(self, x, y, v):
         s='src/tile.png'
-        if v in range (0,5):
-            s='src/' + str(v) + '-tile.png'
+        if v in range (0,5): s='src/' + str(v) + '-tile.png'
         for child in self.children:
-            if (child.ids.x) == x and (child.ids.y) == y and (child.ids.value) != -1:
-                self.remove_widget(child)
-                self.add_widget(BoardTile(x=(x*100), y=(y*100), ids={'x': x, 'y': y, 'value': v, 'object':'tile'}, s=s, v=v, gridX= x, gridY=y, board_instance=self))
-                return
-        self.add_widget(BoardTile(x=(x*100), y=(y*100), ids={'x': x, 'y': y, 'value': v, 'object':'tile'}, s=s, v=v, gridX= x, gridY=y, board_instance=self))
+            if (not isinstance(child, Border)):
+                if (child.ids.x) == x and (child.ids.y) == y and (child.ids.value) != -1:
+                    self.remove_widget(child)
+                    newTile = BoardTile(x, y, v, self)
+                    self.add_widget(newTile)
+                    return newTile
+        newTile = BoardTile(x, y, v, self)
+        self.add_widget(newTile)
+        return newTile
 
 
     def update(self):
         # floor case
         for child in self.children:
-            if (child.ids.y) == 1 and self.border.grid[child.ids.x][0] == child.ids.value and child.ids.object=='tile':
-                points = self.border.grid[child.ids.x][0]
-                print("\n" + str(points) + " points gained. (F)(" + str(child.ids.x) + ",0)")
-                self.score += points
-                self.remove(child.ids.x, child.ids.y)
-                self.gravity(child.ids.x)
-                self.update()
+            if (not isinstance(child, Border)):
+                if (child.ids.y) == 1 and self.border.grid[child.ids.x][0] == child.ids.value and child.ids.object=='tile':
+                    points = self.border.grid[child.ids.x][0]
+                    print("\n" + str(points) + " points gained. (F)(" + str(child.ids.x) + ",0)")
+                    self.score += points
+                    self.remove(child.ids.x, child.ids.y)
+                    self.gravity(child.ids.x)
+                    self.update()
 
 
 class SliqGame(Widget):
     board = ObjectProperty(None)
     score = ObjectProperty(None)
+    
 
     def start(self):
         pass
@@ -328,7 +344,6 @@ class SliqGame(Widget):
     def rotateBorder(self):
         self.board.rotateBorder()
 
-            
 
 class SliqApp(App):
 
